bd176466cee3e38f1c1862f1879dde2b1f13d0a5
Rimantas <belauzas@yahoo.com>
2020-08-04 12:20:43 +0300

    Fun facts animation on scroll

--- js/renderFunFacts.js
+++ js/renderFunFacts.js
@@ -1,10 +1,10 @@
-function renderFunFacts( selector, data ) {
-    const DOM = document.querySelector( selector );
+function renderFunFacts(selector, data) {
+    const DOM = document.querySelector(selector);
 
     let HTML = '';
     const size = data.length;
-    for ( let i=0; i<size; i++ ) {
-        HTML += `<div class="fact col-12 col-md-6 col-lg-4">
+    for (let i = 0; i < size; i++) {
+        HTML += `<div class="fact col-12 col-md-6 col-lg-4" data-animate="true">
                     <p class="number">${data[i].number}</p>
                     <p class="description">${data[i].description}</p>
                 </div>`;
@@ -13,37 +13,51 @@ function renderFunFacts( selector, data ) {
     return DOM.innerHTML = HTML;
 }
 
-function animateFunFacts( maxSeconds ) {
-    const allFacts = document.querySelectorAll('.fact-list > .fact > .number');
-    const size = allFacts.length;
+function animateFunFact(block, maxSeconds) {
+    const numberElement = block.querySelector('.number');
 
-    if ( maxSeconds < 1 ) {
+    if (maxSeconds < 1) {
         throw 'ERROR: mazu maziausiai animacijai reikia 1 sekundes!';
     }
 
-    for ( let i=0; i<size; i++ ) {
-        const number = parseInt( allFacts[i].innerText ),
-            totalTime = maxSeconds % 1 === 0 ? maxSeconds * 1000 : Math.round(maxSeconds) * 1000,
-            fps = 30,
-            timegap = 1000 / fps,                   // galimos tik sveikos sekundes
-            totalFrames = (totalTime / 1000 * fps);
-        let currentNumber = 0,
-            currentFrame = 0;
+    const number = parseInt(numberElement.innerText),
+        totalTime = maxSeconds % 1 === 0 ? maxSeconds * 1000 : Math.round(maxSeconds) * 1000,
+        fps = 30,
+        timegap = 1000 / fps,                   // galimos tik sveikos sekundes
+        totalFrames = (totalTime / 1000 * fps);
+    let currentNumber = 0,
+        currentFrame = 0;
 
-        allFacts[i].innerText = currentNumber;
-        
-        const timer = setInterval( () => {
-            currentFrame++;
-            currentNumber += number / totalFrames;
-            
-            allFacts[i].innerText = Math.round( currentNumber );
-            if ( currentFrame >= totalFrames ) {
-                clearInterval( timer );
-            }
-        }, timegap );
-    }
+    numberElement.innerText = currentNumber;
+
+    const timer = setInterval(() => {
+        currentFrame++;
+        currentNumber += number / totalFrames;
+
+        numberElement.innerText = Math.round(currentNumber);
+        if (currentFrame >= totalFrames) {
+            clearInterval(timer);
+        }
+    }, timegap);
 
     return;
 }
 
-export { renderFunFacts, animateFunFacts };
+function addEventListenerFunFactsOnScroll() {
+    window.addEventListener('scroll', () => {
+        const blocks = document.querySelectorAll('.fact');
+
+        for (const block of blocks) {
+            const blockBottomHeight = block.offsetHeight + block.offsetTop;
+            const windowBottomHeight = window.innerHeight + window.scrollY;
+            if (windowBottomHeight >= blockBottomHeight) {
+                if (block.dataset.animate === 'true') {
+                    animateFunFact(block, 3);
+                    block.dataset.animate = 'false';
+                }
+            }
+        }
+    });
+}
+
+export { renderFunFacts, animateFunFact, addEventListenerFunFactsOnScroll };
