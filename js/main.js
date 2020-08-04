23754074e4bd8d46da33971db49dd5fdd738caad
Rimantas <belauzas@yahoo.com>
2020-08-04 13:48:26 +0300

    Ajax data request

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@
 import data from '../data/data.js';
 
 // import rendering functions
+import { ajax } from './ajax.js';
 import { renderServices } from './renderServices.js';
 import { renderFunFacts, addEventListenerFunFactsOnScroll } from './renderFunFacts.js';
 
 // execute
-const { services, funFacts } = data;
+const { funFacts } = data;
 
-renderServices('#services .services', services);
+ajax(renderServices, '#services .services');
+
+// renderServices('#services .services', services);
 renderFunFacts('#fun_facts > .fact-list', funFacts);
 
 // actions after content rendering
